0cedc9f2e01e82e6c785aae089f2e3369b3d8834
Barabas Raffai <barabas.raffai@gmail.com>
2023-01-14 21:02:25 +0000

    Fix analyzer issues

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,43 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:io';
-import 'dart:convert';
 
-import 'package:flutter/services.dart';
-import 'package:juce_ipc/juce_ipc.dart';
 
 void main() async {
-  print("demo using File");
-  File('/tmp/juce-test-pipe').openRead().transform(utf8.decoder).listen(
-    (String character) {
-      print(character);
-    },
-    onDone: () => print('File StreamSubscription closing'),
-  );
-
-  IOSink writeSink = File('/tmp/juce-test-pipe').openWrite();
-
-  writeSink.write('File 1');
-  writeSink.write('File 2');
-  writeSink.write('File 3');
-  await writeSink.flush();
-  await writeSink.close();
-
-  print("demo using Interprocess");
-  final interprocess = JuceInterprocessConnectionNamedPipe('/tmp/pipe');
-  interprocess.read.transform(utf8.decoder).listen(
-    (String character) {
-      print(character);
-    },
-    onDone: () => print('Interprocess StreamSubscription closing'),
-  );
-
-  interprocess.write.write('Interprocess 4');
-  interprocess.write.write('Interprocess 5');
-  interprocess.write.write('Interprocess 6');
-  await interprocess.write.flush();
-  await interprocess.write.close();
-
   runApp(const MyApp());
 }
 
@@ -61,14 +25,11 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
+        body: const Center(
           child: Text(''),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            final count = _juceIpcPlugin.sayHelloAndReturnCount("Hello World");
-            count.then((count) => debugPrint('count: $count'));
-          },
+          onPressed: () {},
         ),
       ),
     );
--- lib/message_framing.dart
+++ lib/message_framing.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:juce_ipc/message_framing_state.dart';
 import 'dart:typed_data';
 import 'dart:developer';
